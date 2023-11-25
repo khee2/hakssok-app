d69291fc4af13e8920835994efc10333c41f4188
Hagoeun <sera1193@naver.com>
2023-11-25 21:11:28 +0900

    Fix: Scroll error

--- app/src/main/java/com/android/hakssok/DetailActivity.kt
+++ app/src/main/java/com/android/hakssok/DetailActivity.kt
@@ -1,6 +1,7 @@
 package com.android.hakssok
 
 import android.os.Bundle
+import android.text.Layout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
